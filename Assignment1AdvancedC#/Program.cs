285f19ef492b4a0de9047dacd30c3f20374fa8ed
a7medsaber10 <ahmedsa2522003@gmail.com>
2024-07-25 11:40:29 +0300

    Add project files.

---
+++ Assignment1AdvancedC#/Program.cs
@@ -0,0 +1,21 @@
+﻿using Assignment1AdvancedC_.classes;
+using System;
+
+namespace Assignment1AdvancedC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. The Bubble Sort algorithm has a time complexity of O(n^2) in its worst and average cases,
+            //    which makes it inefficient for large datasets. How we can optimize the Bubble Sort algorithm
+            //    And implement the code of this optimized bubble sort algorithm
+            #region Q1(Bubble Sort)
+                //int[] intArray = { 64, 34, 25, 12, 22, 11, 90 };
+                //Console.WriteLine("Unsorted int array: " + string.Join(", ", intArray));
+                //Helper<int>.BubbleSort(intArray);
+                //Console.WriteLine("Sorted int array: " + string.Join(", ", intArray)); 
+                #endregion
+        }
+    }
+}
---
+++ Assignment1AdvancedC#/classes/Helper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1AdvancedC_.classes
+{
+    internal class Helper<T> where T : IComparable<T>
+    {
+        public static void SWAP(ref T x,ref  T y)
+        {
+            T temp = x;
+            x = y;
+            y = temp;
+        }
+
+        public static void BubbleSort(T[] array)
+        {
+            if (array is not null)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = 0; j < array.Length - i - 1; j++)
+                    {
+                        if (array[j].CompareTo(array[j + 1]) == 1)
+                        {
+                            Helper<T>.SWAP(ref array[j], ref array[j + 1]);
+                        }
+                    }
+                }
+            } 
+        }
+    }
+}
